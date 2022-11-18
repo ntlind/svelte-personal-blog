38936644ad2cf7b722d6601ab0a966b68b93bbb7
Nick <nthorlind@gmail.com>
2022-11-18 10:58:50 -0700

    add google fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,25 @@
 <script>
 	import '../app.css';
+	import '@fontsource/questrial';
+	import '@fontsource/inter';
 </script>
 
 <slot />
+
+<style>
+	:global(body) {
+		font-family: 'Inter', sans-serif;
+	}
+	:global(h1),
+	:global(h2) {
+		font-family: 'Questrial', sans-serif;
+	}
+
+	h1 {
+		font-weight: 400;
+		font-size: 800px;
+	}
+	h2 {
+		font-weight: 700;
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				primary: ['Inter'],
+				secondary: ['Questrial']
+			}
+		}
 	},
 	plugins: []
 };
